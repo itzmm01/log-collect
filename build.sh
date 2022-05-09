8929ac467aae95b6e9a7c51638f90dfbd949b234
p_chaooyang <p_chaooyang@tencent.com>
2022-05-09 18:30:57 +0800

    打包使用文档

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ build(){
 	mkdir ./$name-$GOOS-$GOARCH 
 	mv $name ./$name-$GOOS-$GOARCH 
 	cp conf.yml ./$name-$GOOS-$GOARCH 
+	cp README.md ./$name-$GOOS-$GOARCH 
 	tar zcf $name-$GOOS-$GOARCH.tar.gz $name-$GOOS-$GOARCH 
 	rm -rf $name-$GOOS-$GOARCH 
 }
