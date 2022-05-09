b8c0e0919d21a966e06259cd549b14d7d15f992b
p_chaooyang <p_chaooyang@tencent.com>
2022-05-09 18:29:53 +0800

    日志压缩 指定目录存储 pod 关键字匹配 文档完善

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-日志拉取
+## 日志拉取
+
+### 查看支持的日志模块
+
+
+
+```bash
+# 列出所有支持的日志模块
+[root@tcs-172-16-16-50 log-collect-linux-amd64]# ./log-collect -m list
+wemeet-center
+wemeet-conn
+wemeet-xmpp
+cmlb_agent
+cmlb_cc
+logic
+
+Usage: ./log-collect -m get -n xxx
+
+```
+
+### 拉取日志
+
+```bash
+# -m get -n 模块名
+[root@tcs-172-16-16-50 log-collect-linux-amd64]# ./log-collect -m get -n wemeet-center
+2022/05/09 18:26:15 main.go:175: INFO logfile path: /tmp/logs/wemeet-center.tar.gz
+```
+
+*执行完成没有报错会输出压缩后的日志路径，将其下载提供即可*
--- main.go
+++ main.go
@@ -44,6 +44,37 @@ const (
 	GB18030 = Charset("GB18030")
 )
 
+func strip(s_ string, chars_ string) string {
+	s, chars := []rune(s_), []rune(chars_)
+	length := len(s)
+	max := len(s) - 1
+	l, r := true, true //标记当左端或者右端找到正常字符后就停止继续寻找
+	start, end := 0, max
+	tmpEnd := 0
+	charset := make(map[rune]bool) //创建字符集，也就是唯一的字符，方便后面判断是否存在
+	for i := 0; i < len(chars); i++ {
+		charset[chars[i]] = true
+	}
+	for i := 0; i < length; i++ {
+		if _, exist := charset[s[i]]; l && !exist {
+			start = i
+			l = false
+		}
+		tmpEnd = max - i
+		if _, exist := charset[s[tmpEnd]]; r && !exist {
+			end = tmpEnd
+			r = false
+		}
+		if !l && !r {
+			break
+		}
+	}
+	if l && r { // 如果左端和右端都没找到正常字符，那么表示该字符串没有正常字符
+		return ""
+	}
+	return string(s[start : end+1])
+}
+
 // Run
 func Run(command string) (string, error) {
 	var result []byte
@@ -61,9 +92,11 @@ func Run(command string) (string, error) {
 	}
 
 	if err != nil {
-		log.Println("run cmd failed: ", err, ConvertByte2String(result, "GB18030"))
+		msg := fmt.Sprintf("ERROR: cmd(%v), err(%v)", command, ConvertByte2String(result, "GB18030"))
+		log.Println(msg)
 	}
-	return string(result), err
+	resultFormat := strip(string(result), "\n")
+	return resultFormat, err
 }
 
 // ConvertByte2String
@@ -99,25 +132,60 @@ func getLogNameList(conf Config, name string) Log {
 	}
 	return Log{}
 }
-
-func fetchLogFile(conf Log) {
-	// kubectl -n sso cp mariadb-sso-test-ss-0:/workspace/agent  ./agent
-	cmdStr := fmt.Sprintf("kubectl -n %v cp %v:%v/%v %v", conf.NS, conf.Pod, conf.Dir, conf.File, conf.File)
-	if _, err := Run(cmdStr); err != nil {
-		log.Fatalln(err)
-	}
+func checkFileLink() {
 
 }
-func getFileSize(conf Log) string {
-	cmdStr := fmt.Sprintf("kubectl -n %v exec -i %v -- bash -c \"du -k %v/%v|awk '{print \\$1}'\" ", conf.NS, conf.Pod, conf.Dir, conf.File)
+func getAllPod(conf Log) []string {
+	cmdStr := fmt.Sprintf("kubectl -n %v get pod |grep '%v'|awk '{print $1}'", conf.NS, conf.Pod)
+	result, err := Run(cmdStr)
+	if err != nil {
+		return []string{}
+	}
+
+	podName := strings.Split(result, "\n")
+	return podName
+
+}
+func tarFIle(src, dest string) {
+	cmdStr := fmt.Sprintf("tar zcf %v.tar.gz %v --remove-files", src, src)
+	if _, err := Run(cmdStr); err != nil {
+		log.Println("ERROR: zip log ", err)
+	}
+}
+
+func fetchLogFile(conf Log, arg Args) {
+	// kubectl -n sso cp mariadb-sso-test-ss-0:/workspace/agent  ./agent
+	destDir := fmt.Sprintf("%v/%v", *arg.LogDir, conf.Name)
+	if _, err := Run("mkdir -p " + destDir); err != nil {
+		log.Fatalln(err)
+	}
+	podNameList := getAllPod(conf)
+	for _, podName := range podNameList {
+		logFilePath := conf.Dir + "/" + conf.File
+		if checkSpace(conf, arg, logFilePath, podName) {
+			cmdStr := fmt.Sprintf("kubectl -n %v cp %v:%v %v/%v.log", conf.NS, podName, logFilePath, destDir, podName)
+			if _, err := Run(cmdStr); err != nil {
+				log.Println("ERROR: fetch log ", podNameList, podName)
+			}
+		} else {
+			log.Println("ERROR: disk + logfile must < 85%")
+		}
+	}
+	tarFIle(destDir, conf.Name)
+	log.Println(fmt.Sprintf("INFO logfile path: %v.tar.gz", destDir))
+
+}
+func getFileSize(conf Log, logfie, pod string) string {
+	cmdStr := fmt.Sprintf("kubectl -n %v exec -i %v -- bash -c \"du -k %v|awk '{print \\$1}'\" ", conf.NS, pod, logfie)
 	result, err := Run(cmdStr)
 	if err != nil {
 		log.Fatalln("get disk info failed")
 	}
 	return result
 }
-func CurDiskInfo() []string {
-	cmdStr := "df  .|awk 'NR>1{print $2,$3}'"
+func CurDiskInfo(path string) []string {
+	Run("mkdir -p " + path)
+	cmdStr := "df  " + path + "|awk 'NR>1{print $2,$3}'"
 	result, err := Run(cmdStr)
 	if err != nil {
 		log.Fatalln("get disk info failed")
@@ -125,9 +193,9 @@ func CurDiskInfo() []string {
 	diskInfo := strings.Split(result, " ")
 	return diskInfo
 }
-func checkSpace(conf Log) bool {
-	diskInfo := CurDiskInfo()
-	fileSizeStr := getFileSize(conf)
+func checkSpace(conf Log, arg Args, logfie, pod string) bool {
+	diskInfo := CurDiskInfo(*arg.LogDir)
+	fileSizeStr := getFileSize(conf, logfie, pod)
 	fileSize, _ := strconv.ParseInt(fileSizeStr, 10, 64)
 	diskAll, _ := strconv.ParseInt(diskInfo[0], 10, 64)
 	diskUsed, _ := strconv.ParseInt(diskInfo[1], 10, 64)
@@ -143,26 +211,20 @@ func main() {
 
 	arg.Mode = flag.String("m", "", "list all log name")
 	arg.Name = flag.String("n", "", "log name")
-	// arg.Name = flag.String("d", "./logs", "dest logs dir")
+	arg.LogDir = flag.String("d", "/tmp/logs", "dest logs dir")
 	flag.Parse()
 
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
 	conf, err := ReadYamlConfig("conf.yml")
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println(*arg.Mode, *arg.Name)
 	if *arg.Mode == "get" {
-		//  kubectl exec -i mvlog-server-0 -- bash -c "ls -l /data/spp_mvlog_server/data/wemeet_conn|awk 'NR>1{print \$5}'"
 		logInfo := getLogNameList(*conf, *arg.Name)
 		if logInfo.Name == "" {
 			log.Println("not found log: ", *arg.Name)
 		} else {
-			if checkSpace(logInfo) {
-				fetchLogFile(logInfo)
-			} else {
-				log.Fatalln("disk space not enough: used + logfile  < 85% ")
-			}
-
+			fetchLogFile(logInfo, arg)
 		}
 
 	} else if *arg.Mode == "list" {
